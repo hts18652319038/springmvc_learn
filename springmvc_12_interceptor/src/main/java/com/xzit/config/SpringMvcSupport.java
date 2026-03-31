bc95d04cc7153f5e15b17607017ed44d701af4f8
Tongshun Huang <151819258+hts18652319038@users.noreply.github.com>
2026-03-31 14:54:40 +0800

    Add files via upload

---
+++ springmvc_12_interceptor/src/main/java/com/xzit/config/SpringMvcSupport.java
@@ -0,0 +1,28 @@
+package com.xzit.config;
+
+import com.xzit.controller.interceptor.ProjectInterceptor;
+import com.xzit.controller.interceptor.ProjectInterceptor2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class SpringMvcSupport extends WebMvcConfigurationSupport {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+    @Autowired
+    private ProjectInterceptor2 projectInterceptor2;
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books","/books/*");
+        registry.addInterceptor(projectInterceptor2).addPathPatterns("/books","/books/*");
+    }
+}
---
+++ springmvc_12_interceptor/src/main/java/com/xzit/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.xzit.controller;
+
+import com.xzit.domain.Book;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @PostMapping
+    public String save(@RequestBody Book book) {
+        System.out.println("book save..."+book);
+        return "{'module':'book save'}";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Integer id) {
+        System.out.println("book delete..."+id);
+        return "{'module':'book delete'}";
+    }
+
+    @PutMapping
+    public String update(@RequestBody Book book) {
+        System.out.println("book update..."+book);
+        return "{'module':'book update'}";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Integer id) {
+        System.out.println("book getById..."+id);
+        return "{'module':'book getById'}";
+    }
+}
